994d670aa20514026ec2f73d84b2ecbe267fa52a
Biplab-Dutta <biplabdutta860@gmail.com>
2021-04-07 10:25:59 +0545

    Made few changes in the solution of question 6

--- Question_6.js
+++ Question_6.js
@@ -1,11 +1,17 @@
 var number = prompt("Enter the number.");
-var isPrime = true;
 
-for (var i = 2; i < number; i++) {
-    if (number % i == 0) {
-        console.log("Composite Number.");
-        isPrime = false;
-        break;
+if (number == 0 || number == 1) {
+    console.log("The given input is neither prime nor composite.");
+}
+else {
+    var isPrime = true;
+
+    for (var i = 2; i < number; i++) {
+        if (number % i == 0) {
+            console.log("Composite Number.");
+            isPrime = false;
+            break;
+        }
     }
 }
 if (isPrime == true) {
